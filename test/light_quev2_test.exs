603001b633f45bd654e07c2e167ec6ebd20187cc
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2018-12-08 13:58:53 +0200

    Test. Ack check empty que, check persistance status

--- test/light_quev2_test.exs
+++ test/light_quev2_test.exs
@@ -12,15 +12,18 @@ defmodule LightQuev2Test do
 
   describe "add/1" do
 
-      test "Que add should return the tuple {:ok, enqueued}" do
-        new_job = "job free text"
-        assert LightQuev2.add(new_job) == {:ok, :enqueued}
+      test "should return the tuple {:ok, enqueued}" do
+        assert LightQuev2.add("task1") == {:ok, :enqueued}
+      end
+
+      test "should validate input param" do
+        assert LightQuev2.add() == {:error, :job_is_empty}
       end
   end
 
   describe "get/1" do
 
-    test "Que add/get should be first in / first out" do
+    test "add/get should be first in / first out" do
 
       LightQuev2.add("task1")
       LightQuev2.add("task2")
@@ -35,7 +38,7 @@ defmodule LightQuev2Test do
 
   describe "reject/1" do
 
-    test "Que reject should push in the end of queue" do
+    test "should push in the end of queue" do
 
       LightQuev2.add("task1")
       LightQuev2.add("task2")
@@ -50,7 +53,7 @@ defmodule LightQuev2Test do
       assert "task2" == task2.task
     end
 
-    test "Que reject should change status in persitence" do
+    test "should change status in persitence" do
 
 
       LightQuev2.add("task1")
@@ -67,7 +70,7 @@ defmodule LightQuev2Test do
 
   describe "ack/1" do
 
-    test "Que ack should remove from persitence job" do
+    test "should remove from persitence job" do
 
       LightQuev2.add("task1")
       task1 = LightQuev2.get()
@@ -75,6 +78,24 @@ defmodule LightQuev2Test do
       LightQuev2.ack(task1.id)
 
       assert Persistence.get_task_list() == []
+    end
+
+    test "should change status in persistence" do
+
+      LightQuev2.add("task1")
+      task = LightQuev2.get()
+      LightQuev2.ack(task.id)
+
+      assert (Persistence.get(task.id)).status == :ack
+    end
+
+    test "should remove job from que" do
+
+      LightQuev2.add("task1")
+      task = LightQuev2.get()
+      LightQuev2.ack(task.id)
+
+      assert LightQuev2.get() == {:ok, :queue_empty}
     end
   end
 end
