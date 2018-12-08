8b74687e54f29b829774ad20d86bd48c0fef7fb6
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2018-12-08 12:13:59 +0200

    Test. Que add/get should be first in / first out

--- test/light_quev2_test.exs
+++ test/light_quev2_test.exs
@@ -7,4 +7,14 @@ defmodule LightQuev2Test do
     {:ok, _pid} = LightQuev2.start_link()
     assert LightQuev2.add(new_job) == {:ok, :enqueued}
   end
+
+  test "Que add/get should be first in / first out" do
+    {:ok, _pid} = LightQuev2.start_link()
+    LightQuev2.add("task1")
+    LightQuev2.add("task2")
+    task1 = LightQuev2.get()
+    task2 = LightQuev2.get()
+    assert "task1" == task1[:task]
+    assert "task2" == task1[:task]
+  end
 end
