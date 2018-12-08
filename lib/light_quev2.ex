967ebcc434873cb174d50bbbdec28fb59c553dbf
Zheludkov Artur <a.zheludkov@ticketstravelnetwork.com>
2018-12-08 10:37:19 +0200

    initial commit

---
+++ lib/light_quev2.ex
@@ -0,0 +1,18 @@
+defmodule LightQuev2 do
+  @moduledoc """
+  Documentation for LightQuev2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LightQuev2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/light_quev2_test.exs
@@ -0,0 +1,8 @@
+defmodule LightQuev2Test do
+  use ExUnit.Case
+  doctest LightQuev2
+
+  test "greets the world" do
+    assert LightQuev2.hello() == :world
+  end
+end
